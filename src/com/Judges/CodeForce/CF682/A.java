8465c19d1abfaec11c97fd7b4a81573fcd7112c9
Reza Motlagh <rmoti49@gmail.com>
2019-06-20 01:23:45 +0300

    One Problem Solved

--- src/com/Judges/CodeForce/CF682/A.java
+++ src/com/Judges/CodeForce/CF682/A.java
@@ -4,31 +4,22 @@ import java.util.Scanner;
 import java.util.zip.CheckedOutputStream;
 
 public class A {
-    int MAX = (int)10e6;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int count = 0;
-        int MAX = Math.max(A,B);
-        int MIN = Math.min(A,B);
-        int[] fsum = new int[5];
-        int[] ssum = new int[5];
-
-        for(int i = 1; i <= A ; i++)
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int A = Math.max(a,b);
+        int B = Math.min(a,b);
+        long count = 0;
+        for(int i = 1 ; i <= A ; i++)
         {
-            fsum[i%5]++;
+            int mod = i % 5;
+            if(mod>=B)
+                continue;
+            count+=(B - (5-mod))/5;
+            count++;
         }
-        for(int i = 1 ; i <= B ; i++)
-        {
-            ssum[4 - (i%5)]++;
-        }
-        long fi = 0;
-        for(int i = 0 ; i< 5 ;i++)
-        {
-            fi += ssum[i]*fsum[i];
-        }
-        System.out.println(fi);
+        System.out.println(count);
     }
 
 }
